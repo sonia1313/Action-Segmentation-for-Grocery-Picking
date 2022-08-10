cddaf80e57c06140b348adf3de78ff4575dc58c9
sonia <sonia.mathews@live.com>
2022-08-10 13:48:10 +0100

    data saved as tensors

---
+++ utils/tactile_get_dataset_utils.py
@@ -0,0 +1,54 @@
+""""
+Author - Sonia Mathews
+tactile_get_dataset_utils.py
+
+Script to create pickle files of datasets at different fps,
+to improve efficiency during experimentation
+"""
+import os
+
+import torch
+import pickle
+
+
+from utils.preprocessing import encode_labels, pad_data, standardise_features, normalize_features
+
+# preprocess_config = {
+#     'data_path': 'dataset',  # get_files()
+#     'clutter': True,  # get_files()
+#     'single': True,  # get_files()
+#     'frames_per_sec': 15, #6 #3 #1 #15  #read_data()
+#     'feature_engineering': True,  # True #read_data()
+#     'standardise_data': False,  # preprocess_dataset()
+#     'normalize_data': True,  # preprocess_dataset()
+#     'pad_data': True}  # preprocess_dataset()
+
+def get_dataset():
+    os.chdir("C:/Users/sonia/OneDrive - Queen Mary, University of London/Action-Segmentation-Project")
+    print(os.getcwd())
+    #3fps_clutter_single
+
+    with open('data/tactile/with_feature_engineering/3fps/single_tactile_df.pkl', 'rb') as f:
+        single_dfs = pickle.load(f)
+        # print(len(frames))
+        print(single_dfs[0].head())
+        actions_per_seq, label_to_index_map = encode_labels(single_dfs)
+
+        X_data, y_data = pad_data(single_dfs, actions_per_seq, n_sequences= len(single_dfs), features=['index','middle','thumb'])
+
+    print(type(X_data))
+    print(type(y_data))
+    print(f"no_sequences:{len(X_data)}")
+
+    torch.save((X_data,y_data), 'data/tactile/with_feature_engineering/3fps/single_3fps_dataset.pt')
+    X, y = torch.load('data/tactile/with_feature_engineering/3fps/single_3fps_dataset.pt')
+    print(X.shape)
+    print(y.shape)
+    print(X[0])
+    print(y[0])
+    print(len(X[0]))
+    print(len(y[0]))
+
+
+if __name__ == '__main__':
+    get_dataset()
